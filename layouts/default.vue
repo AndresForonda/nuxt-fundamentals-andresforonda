9a2ee8ad937d80ac755dd6561d04c531aa040c59
Andrés Fernando Foronda Espinal <andresforonda.af@gmail.com>
2019-06-09 13:07:57 -0400

    init commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <the-nav-bar />
+    <nuxt />
+  </div>
+</template>
+<script>
+import TheNavBar from '@/components/TheNavBar'
+export default {
+  components: {
+    TheNavBar
+  }
+}
+</script>
